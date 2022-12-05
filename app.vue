9dc06336dba01c2ab49db885d1176536af5288f3
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-12-05 19:56:29 -0300

    Initial nuxt files and dependencies

---
+++ app.vue
@@ -0,0 +1,25 @@
+<script setup>
+const basic = ref(false);
+</script>
+
+<template>
+  <h1>CSS Reset Boiler Plate</h1>
+  <div class="grid">
+    <div>
+      <label for="switch">
+        <input
+          type="checkbox"
+          id="switch"
+          name="switch"
+          role="switch"
+          v-model="basic"
+        />
+        Border reset
+      </label>
+    </div>
+
+    <template v-if="basic">
+      <code>Something</code>
+    </template>
+  </div>
+</template>
