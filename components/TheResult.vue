28eaf61d1193ea9d936ac0c4887e378b2237e4d5
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-12-23 14:56:14 -0300

    Prism syntax highlights

--- components/TheResult.vue
+++ components/TheResult.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
 import { useOptionsStore } from "@/stores/options";
+import "../prism/prism.js";
+import "../prism/prism.css";
 
 const options = useOptionsStore();
 </script>
 <template>
-  <code>
-    <!-- Basic -->
-    <template v-if="options.basic"> Something </template>
-
-    <!-- Reset Link -->
-    <template v-if="options.resetLinkDecoration">
-      <pre>
-a<template v-if="options.resetLinkDecorationWithoutClass">:not([class])</template> {
-  text-decoration-skip-ink: auto;
-}     </pre>
-    </template>
-  </code>
+  <pre>
+    <code class="language-css" v-show="options.basic">
+    p { 
+      color: red 
+    }
+    </code>
+    <code class="language-css" v-show="options.resetLinkDecoration">
+    a {
+      text-decoration-skip-ink: auto;
+    }
+    </code>
+  </pre>
 </template>
